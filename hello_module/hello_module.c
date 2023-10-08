3e8f164a4374004735e02b2e53bb64174de3d392
Charles Dias <charlesdias.cd@outlook.com>
2023-10-08 08:32:24 -0300

    refactor: hello module

--- hello_module/hello_module.c
+++ hello_module/hello_module.c
@@ -1,24 +1,24 @@
 #include <linux/module.h> /* Needed by all modules */
 #include <linux/kernel.h> /* Needed for KERN_INFO */
-#include <linux/init.h>   /* Needed for the macros */
+#include <linux/init.h>   /* Needed for the __init and __exit macros */
 
-static int __init hello_start(void)
+static int __init hello_init(void)
 {
     printk(KERN_INFO "Loading hello module...\n");
-    printk(KERN_INFO "Hello, World!\n");
+    pr_info("Hello, World!\n"); // It is the same as printk(KERN_INFO "Hello, World!\n");
     return 0;
 }
 
-static void __exit hello_end(void)
+static void __exit hello_exit(void)
 {
-    printk(KERN_INFO "The module is now unloaded!\n");
+    pr_info("The module is now unloaded!\n"); // It is the same as printk(KERN_INFO "The module is now unloaded!\n");
     printk(KERN_INFO "Goodbye, World!\n");
 }
 
-module_init(hello_start);
-module_exit(hello_end);
+module_init(hello_init);
+module_exit(hello_exit);
 
-MODULE_AUTHOR("Charles Dias");
+MODULE_AUTHOR("Charles Dias <charlesdias.cd@outlook.com>");
 MODULE_DESCRIPTION("A Hello, World Module");
 MODULE_LICENSE("GPL");
 MODULE_VERSION("0.1");
